3a0b42c9bc0fc20061bd9576cc6683f385c68778
Antoine Dupuis <antoine.dupuis@datadoghq.com>
2018-01-29 11:37:10 +0100

    Set protoc version to 3.5.1, gen using rake.

--- generate.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-protoc --proto_path=$GOPATH/src:. --gogofast_out=. agent_payload.proto
-# Go files will be written to the folder defined in the `go_package` option.
-# So we need to move them into the correct folder.
-cp ./github.com/DataDog/agent-payload/gogen/* gogen/
-rm -rf ./github.com/
-
-protoc --proto_path=$GOPATH/src:. --python_out=python agent_payload.proto
