5c48f5a44624dd8d4b41e64011fc515447a62309
Alexandre Yang <alexandre.yang@datadoghq.com>
2021-05-03 11:20:56 +0200

    Update types

--- network-devices/agent.pb.go
+++ network-devices/agent.pb.go
@@ -33,9 +33,7 @@ var _ = math.Inf
 const _ = proto.GoGoProtoPackageIsVersion2 // please upgrade the proto package
 
 type CollectorNetworkDevice struct {
-	AutodiscoverySubnet string           `protobuf:"bytes,1,opt,name=autodiscoverySubnet,proto3" json:"autodiscoverySubnet,omitempty"`
-	Devices             []*NetworkDevice `protobuf:"bytes,2,rep,name=devices" json:"devices,omitempty"`
-	Tags                []string         `protobuf:"bytes,3,rep,name=tags" json:"tags,omitempty"`
+	Device *NetworkDevice `protobuf:"bytes,1,opt,name=device" json:"device,omitempty"`
 }
 
 func (m *CollectorNetworkDevice) Reset()                    { *m = CollectorNetworkDevice{} }
@@ -43,23 +41,9 @@ func (m *CollectorNetworkDevice) String() string            { return proto.Compa
 func (*CollectorNetworkDevice) ProtoMessage()               {}
 func (*CollectorNetworkDevice) Descriptor() ([]byte, []int) { return fileDescriptorAgent, []int{0} }
 
-func (m *CollectorNetworkDevice) GetAutodiscoverySubnet() string {
+func (m *CollectorNetworkDevice) GetDevice() *NetworkDevice {
 	if m != nil {
-		return m.AutodiscoverySubnet
-	}
-	return ""
-}
-
-func (m *CollectorNetworkDevice) GetDevices() []*NetworkDevice {
-	if m != nil {
-		return m.Devices
-	}
-	return nil
-}
-
-func (m *CollectorNetworkDevice) GetTags() []string {
-	if m != nil {
-		return m.Tags
+		return m.Device
 	}
 	return nil
 }
@@ -89,14 +73,16 @@ func (m *CollectorNetworkInterface) GetInterfaces() []*NetworkInterface {
 }
 
 type NetworkDevice struct {
-	Id          string   `protobuf:"bytes,1,opt,name=id,proto3" json:"id,omitempty"`
-	Name        string   `protobuf:"bytes,2,opt,name=name,proto3" json:"name,omitempty"`
-	Description string   `protobuf:"bytes,3,opt,name=description,proto3" json:"description,omitempty"`
-	IpAddress   string   `protobuf:"bytes,4,opt,name=ipAddress,proto3" json:"ipAddress,omitempty"`
-	SysObjectId string   `protobuf:"bytes,5,opt,name=sysObjectId,proto3" json:"sysObjectId,omitempty"`
-	Profile     string   `protobuf:"bytes,6,opt,name=profile,proto3" json:"profile,omitempty"`
-	Vendor      string   `protobuf:"bytes,7,opt,name=vendor,proto3" json:"vendor,omitempty"`
-	Tags        []string `protobuf:"bytes,8,rep,name=tags" json:"tags,omitempty"`
+	Id                  string   `protobuf:"bytes,1,opt,name=id,proto3" json:"id,omitempty"`
+	IdTags              []string `protobuf:"bytes,2,rep,name=id_tags,json=idTags" json:"id_tags,omitempty"`
+	Name                string   `protobuf:"bytes,3,opt,name=name,proto3" json:"name,omitempty"`
+	Description         string   `protobuf:"bytes,4,opt,name=description,proto3" json:"description,omitempty"`
+	IpAddress           string   `protobuf:"bytes,5,opt,name=ipAddress,proto3" json:"ipAddress,omitempty"`
+	SysObjectId         string   `protobuf:"bytes,6,opt,name=sysObjectId,proto3" json:"sysObjectId,omitempty"`
+	Profile             string   `protobuf:"bytes,7,opt,name=profile,proto3" json:"profile,omitempty"`
+	Vendor              string   `protobuf:"bytes,8,opt,name=vendor,proto3" json:"vendor,omitempty"`
+	AutodiscoverySubnet string   `protobuf:"bytes,9,opt,name=autodiscoverySubnet,proto3" json:"autodiscoverySubnet,omitempty"`
+	Tags                []string `protobuf:"bytes,10,rep,name=tags" json:"tags,omitempty"`
 }
 
 func (m *NetworkDevice) Reset()                    { *m = NetworkDevice{} }
@@ -109,6 +95,13 @@ func (m *NetworkDevice) GetId() string {
 		return m.Id
 	}
 	return ""
+}
+
+func (m *NetworkDevice) GetIdTags() []string {
+	if m != nil {
+		return m.IdTags
+	}
+	return nil
 }
 
 func (m *NetworkDevice) GetName() string {
@@ -149,6 +142,13 @@ func (m *NetworkDevice) GetProfile() string {
 func (m *NetworkDevice) GetVendor() string {
 	if m != nil {
 		return m.Vendor
+	}
+	return ""
+}
+
+func (m *NetworkDevice) GetAutodiscoverySubnet() string {
+	if m != nil {
+		return m.AutodiscoverySubnet
 	}
 	return ""
 }
@@ -245,38 +245,15 @@ func (m *CollectorNetworkDevice) MarshalTo(dAtA []byte) (int, error) {
 	_ = i
 	var l int
 	_ = l
-	if len(m.AutodiscoverySubnet) > 0 {
+	if m.Device != nil {
 		dAtA[i] = 0xa
 		i++
-		i = encodeVarintAgent(dAtA, i, uint64(len(m.AutodiscoverySubnet)))
-		i += copy(dAtA[i:], m.AutodiscoverySubnet)
-	}
-	if len(m.Devices) > 0 {
-		for _, msg := range m.Devices {
-			dAtA[i] = 0x12
-			i++
-			i = encodeVarintAgent(dAtA, i, uint64(msg.Size()))
-			n, err := msg.MarshalTo(dAtA[i:])
-			if err != nil {
-				return 0, err
-			}
-			i += n
+		i = encodeVarintAgent(dAtA, i, uint64(m.Device.Size()))
+		n1, err := m.Device.MarshalTo(dAtA[i:])
+		if err != nil {
+			return 0, err
 		}
-	}
-	if len(m.Tags) > 0 {
-		for _, s := range m.Tags {
-			dAtA[i] = 0x1a
-			i++
-			l = len(s)
-			for l >= 1<<7 {
-				dAtA[i] = uint8(uint64(l)&0x7f | 0x80)
-				l >>= 7
-				i++
-			}
-			dAtA[i] = uint8(l)
-			i++
-			i += copy(dAtA[i:], s)
-		}
+		i += n1
 	}
 	return i, nil
 }
@@ -338,45 +315,66 @@ func (m *NetworkDevice) MarshalTo(dAtA []byte) (int, error) {
 		i = encodeVarintAgent(dAtA, i, uint64(len(m.Id)))
 		i += copy(dAtA[i:], m.Id)
 	}
+	if len(m.IdTags) > 0 {
+		for _, s := range m.IdTags {
+			dAtA[i] = 0x12
+			i++
+			l = len(s)
+			for l >= 1<<7 {
+				dAtA[i] = uint8(uint64(l)&0x7f | 0x80)
+				l >>= 7
+				i++
+			}
+			dAtA[i] = uint8(l)
+			i++
+			i += copy(dAtA[i:], s)
+		}
+	}
 	if len(m.Name) > 0 {
-		dAtA[i] = 0x12
+		dAtA[i] = 0x1a
 		i++
 		i = encodeVarintAgent(dAtA, i, uint64(len(m.Name)))
 		i += copy(dAtA[i:], m.Name)
 	}
 	if len(m.Description) > 0 {
-		dAtA[i] = 0x1a
+		dAtA[i] = 0x22
 		i++
 		i = encodeVarintAgent(dAtA, i, uint64(len(m.Description)))
 		i += copy(dAtA[i:], m.Description)
 	}
 	if len(m.IpAddress) > 0 {
-		dAtA[i] = 0x22
+		dAtA[i] = 0x2a
 		i++
 		i = encodeVarintAgent(dAtA, i, uint64(len(m.IpAddress)))
 		i += copy(dAtA[i:], m.IpAddress)
 	}
 	if len(m.SysObjectId) > 0 {
-		dAtA[i] = 0x2a
+		dAtA[i] = 0x32
 		i++
 		i = encodeVarintAgent(dAtA, i, uint64(len(m.SysObjectId)))
 		i += copy(dAtA[i:], m.SysObjectId)
 	}
 	if len(m.Profile) > 0 {
-		dAtA[i] = 0x32
+		dAtA[i] = 0x3a
 		i++
 		i = encodeVarintAgent(dAtA, i, uint64(len(m.Profile)))
 		i += copy(dAtA[i:], m.Profile)
 	}
 	if len(m.Vendor) > 0 {
-		dAtA[i] = 0x3a
+		dAtA[i] = 0x42
 		i++
 		i = encodeVarintAgent(dAtA, i, uint64(len(m.Vendor)))
 		i += copy(dAtA[i:], m.Vendor)
 	}
+	if len(m.AutodiscoverySubnet) > 0 {
+		dAtA[i] = 0x4a
+		i++
+		i = encodeVarintAgent(dAtA, i, uint64(len(m.AutodiscoverySubnet)))
+		i += copy(dAtA[i:], m.AutodiscoverySubnet)
+	}
 	if len(m.Tags) > 0 {
 		for _, s := range m.Tags {
-			dAtA[i] = 0x42
+			dAtA[i] = 0x52
 			i++
 			l = len(s)
 			for l >= 1<<7 {
@@ -461,21 +459,9 @@ func encodeVarintAgent(dAtA []byte, offset int, v uint64) int {
 func (m *CollectorNetworkDevice) Size() (n int) {
 	var l int
 	_ = l
-	l = len(m.AutodiscoverySubnet)
-	if l > 0 {
+	if m.Device != nil {
+		l = m.Device.Size()
 		n += 1 + l + sovAgent(uint64(l))
-	}
-	if len(m.Devices) > 0 {
-		for _, e := range m.Devices {
-			l = e.Size()
-			n += 1 + l + sovAgent(uint64(l))
-		}
-	}
-	if len(m.Tags) > 0 {
-		for _, s := range m.Tags {
-			l = len(s)
-			n += 1 + l + sovAgent(uint64(l))
-		}
 	}
 	return n
 }
@@ -503,6 +489,12 @@ func (m *NetworkDevice) Size() (n int) {
 	if l > 0 {
 		n += 1 + l + sovAgent(uint64(l))
 	}
+	if len(m.IdTags) > 0 {
+		for _, s := range m.IdTags {
+			l = len(s)
+			n += 1 + l + sovAgent(uint64(l))
+		}
+	}
 	l = len(m.Name)
 	if l > 0 {
 		n += 1 + l + sovAgent(uint64(l))
@@ -524,6 +516,10 @@ func (m *NetworkDevice) Size() (n int) {
 		n += 1 + l + sovAgent(uint64(l))
 	}
 	l = len(m.Vendor)
+	if l > 0 {
+		n += 1 + l + sovAgent(uint64(l))
+	}
+	l = len(m.AutodiscoverySubnet)
 	if l > 0 {
 		n += 1 + l + sovAgent(uint64(l))
 	}
@@ -611,36 +607,7 @@ func (m *CollectorNetworkDevice) Unmarshal(dAtA []byte) error {
 		switch fieldNum {
 		case 1:
 			if wireType != 2 {
-				return fmt.Errorf("proto: wrong wireType = %d for field AutodiscoverySubnet", wireType)
-			}
-			var stringLen uint64
-			for shift := uint(0); ; shift += 7 {
-				if shift >= 64 {
-					return ErrIntOverflowAgent
-				}
-				if iNdEx >= l {
-					return io.ErrUnexpectedEOF
-				}
-				b := dAtA[iNdEx]
-				iNdEx++
-				stringLen |= (uint64(b) & 0x7F) << shift
-				if b < 0x80 {
-					break
-				}
-			}
-			intStringLen := int(stringLen)
-			if intStringLen < 0 {
-				return ErrInvalidLengthAgent
-			}
-			postIndex := iNdEx + intStringLen
-			if postIndex > l {
-				return io.ErrUnexpectedEOF
-			}
-			m.AutodiscoverySubnet = string(dAtA[iNdEx:postIndex])
-			iNdEx = postIndex
-		case 2:
-			if wireType != 2 {
-				return fmt.Errorf("proto: wrong wireType = %d for field Devices", wireType)
+				return fmt.Errorf("proto: wrong wireType = %d for field Device", wireType)
 			}
 			var msglen int
 			for shift := uint(0); ; shift += 7 {
@@ -664,39 +631,12 @@ func (m *CollectorNetworkDevice) Unmarshal(dAtA []byte) error {
 			if postIndex > l {
 				return io.ErrUnexpectedEOF
 			}
-			m.Devices = append(m.Devices, &NetworkDevice{})
-			if err := m.Devices[len(m.Devices)-1].Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
+			if m.Device == nil {
+				m.Device = &NetworkDevice{}
+			}
+			if err := m.Device.Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
 				return err
 			}
-			iNdEx = postIndex
-		case 3:
-			if wireType != 2 {
-				return fmt.Errorf("proto: wrong wireType = %d for field Tags", wireType)
-			}
-			var stringLen uint64
-			for shift := uint(0); ; shift += 7 {
-				if shift >= 64 {
-					return ErrIntOverflowAgent
-				}
-				if iNdEx >= l {
-					return io.ErrUnexpectedEOF
-				}
-				b := dAtA[iNdEx]
-				iNdEx++
-				stringLen |= (uint64(b) & 0x7F) << shift
-				if b < 0x80 {
-					break
-				}
-			}
-			intStringLen := int(stringLen)
-			if intStringLen < 0 {
-				return ErrInvalidLengthAgent
-			}
-			postIndex := iNdEx + intStringLen
-			if postIndex > l {
-				return io.ErrUnexpectedEOF
-			}
-			m.Tags = append(m.Tags, string(dAtA[iNdEx:postIndex]))
 			iNdEx = postIndex
 		default:
 			iNdEx = preIndex
@@ -889,6 +829,35 @@ func (m *NetworkDevice) Unmarshal(dAtA []byte) error {
 			iNdEx = postIndex
 		case 2:
 			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field IdTags", wireType)
+			}
+			var stringLen uint64
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowAgent
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				stringLen |= (uint64(b) & 0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			intStringLen := int(stringLen)
+			if intStringLen < 0 {
+				return ErrInvalidLengthAgent
+			}
+			postIndex := iNdEx + intStringLen
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.IdTags = append(m.IdTags, string(dAtA[iNdEx:postIndex]))
+			iNdEx = postIndex
+		case 3:
+			if wireType != 2 {
 				return fmt.Errorf("proto: wrong wireType = %d for field Name", wireType)
 			}
 			var stringLen uint64
@@ -916,7 +885,7 @@ func (m *NetworkDevice) Unmarshal(dAtA []byte) error {
 			}
 			m.Name = string(dAtA[iNdEx:postIndex])
 			iNdEx = postIndex
-		case 3:
+		case 4:
 			if wireType != 2 {
 				return fmt.Errorf("proto: wrong wireType = %d for field Description", wireType)
 			}
@@ -945,7 +914,7 @@ func (m *NetworkDevice) Unmarshal(dAtA []byte) error {
 			}
 			m.Description = string(dAtA[iNdEx:postIndex])
 			iNdEx = postIndex
-		case 4:
+		case 5:
 			if wireType != 2 {
 				return fmt.Errorf("proto: wrong wireType = %d for field IpAddress", wireType)
 			}
@@ -974,7 +943,7 @@ func (m *NetworkDevice) Unmarshal(dAtA []byte) error {
 			}
 			m.IpAddress = string(dAtA[iNdEx:postIndex])
 			iNdEx = postIndex
-		case 5:
+		case 6:
 			if wireType != 2 {
 				return fmt.Errorf("proto: wrong wireType = %d for field SysObjectId", wireType)
 			}
@@ -1003,7 +972,7 @@ func (m *NetworkDevice) Unmarshal(dAtA []byte) error {
 			}
 			m.SysObjectId = string(dAtA[iNdEx:postIndex])
 			iNdEx = postIndex
-		case 6:
+		case 7:
 			if wireType != 2 {
 				return fmt.Errorf("proto: wrong wireType = %d for field Profile", wireType)
 			}
@@ -1032,7 +1001,7 @@ func (m *NetworkDevice) Unmarshal(dAtA []byte) error {
 			}
 			m.Profile = string(dAtA[iNdEx:postIndex])
 			iNdEx = postIndex
-		case 7:
+		case 8:
 			if wireType != 2 {
 				return fmt.Errorf("proto: wrong wireType = %d for field Vendor", wireType)
 			}
@@ -1061,7 +1030,36 @@ func (m *NetworkDevice) Unmarshal(dAtA []byte) error {
 			}
 			m.Vendor = string(dAtA[iNdEx:postIndex])
 			iNdEx = postIndex
-		case 8:
+		case 9:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field AutodiscoverySubnet", wireType)
+			}
+			var stringLen uint64
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowAgent
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				stringLen |= (uint64(b) & 0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			intStringLen := int(stringLen)
+			if intStringLen < 0 {
+				return ErrInvalidLengthAgent
+			}
+			postIndex := iNdEx + intStringLen
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.AutodiscoverySubnet = string(dAtA[iNdEx:postIndex])
+			iNdEx = postIndex
+		case 10:
 			if wireType != 2 {
 				return fmt.Errorf("proto: wrong wireType = %d for field Tags", wireType)
 			}
@@ -1442,33 +1440,33 @@ var (
 func init() { proto.RegisterFile("proto/network-devices/agent.proto", fileDescriptorAgent) }
 
 var fileDescriptorAgent = []byte{
-	// 436 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x74, 0x93, 0xcf, 0x6e, 0x13, 0x31,
-	0x10, 0xc6, 0xe5, 0x24, 0x9b, 0x34, 0x53, 0xf1, 0x47, 0xa6, 0xaa, 0x0c, 0x42, 0xab, 0x34, 0xa7,
-	0x5c, 0x9a, 0x54, 0x70, 0xe1, 0x84, 0x54, 0xc8, 0x25, 0x17, 0x90, 0xb6, 0x37, 0x2e, 0xc8, 0xb1,
-	0xa7, 0xc1, 0xb0, 0x6b, 0xaf, 0x6c, 0x6f, 0x20, 0x3c, 0x09, 0x8f, 0xc4, 0x91, 0x13, 0x17, 0x2e,
-	0x28, 0x4f, 0x82, 0xd6, 0xbb, 0x1b, 0xb6, 0xdb, 0xf6, 0xe6, 0xf9, 0x7e, 0x33, 0xe3, 0xf9, 0x46,
-	0x1a, 0x38, 0xcb, 0xad, 0xf1, 0x66, 0xa1, 0xd1, 0x7f, 0x35, 0xf6, 0xcb, 0xb9, 0xc4, 0xad, 0x12,
-	0xe8, 0x16, 0x7c, 0x83, 0xda, 0xcf, 0x03, 0xa3, 0x8f, 0x6a, 0xf8, 0xb1, 0x86, 0xd3, 0x1f, 0x04,
-	0x4e, 0xdf, 0x9a, 0x34, 0x45, 0xe1, 0x8d, 0x7d, 0x57, 0xc1, 0x65, 0x60, 0xf4, 0x02, 0x9e, 0xf0,
-	0xc2, 0x1b, 0xa9, 0x9c, 0x30, 0x5b, 0xb4, 0xbb, 0xab, 0x62, 0xad, 0xd1, 0x33, 0x32, 0x21, 0xb3,
-	0x71, 0x72, 0x17, 0xa2, 0xaf, 0x60, 0x54, 0xf7, 0x65, 0xbd, 0x49, 0x7f, 0x76, 0xfc, 0x22, 0x9e,
-	0x77, 0xfe, 0x9b, 0xdf, 0xf8, 0x22, 0x69, 0xd2, 0x29, 0x85, 0x81, 0xe7, 0x1b, 0xc7, 0xfa, 0x93,
-	0xfe, 0x6c, 0x9c, 0x84, 0xf7, 0xf4, 0x3b, 0x3c, 0xed, 0x4e, 0xb6, 0xd2, 0x1e, 0xed, 0x35, 0x17,
-	0x48, 0x9f, 0xc1, 0x51, 0x55, 0xbb, 0x92, 0xf5, 0x44, 0x87, 0x98, 0x5e, 0x02, 0xa8, 0x26, 0xb1,
-	0x99, 0xe4, 0xec, 0xbe, 0x49, 0x0e, 0x2d, 0x93, 0x56, 0xd1, 0xf4, 0x0f, 0x81, 0x07, 0x37, 0xb7,
-	0xf1, 0x10, 0x7a, 0xaa, 0xf9, 0xaa, 0xa7, 0x64, 0x39, 0xb1, 0xe6, 0x19, 0xb2, 0x5e, 0x50, 0xc2,
-	0x9b, 0x4e, 0xe0, 0x58, 0xa2, 0x13, 0x56, 0xe5, 0x5e, 0x19, 0xcd, 0xfa, 0x01, 0xb5, 0x25, 0xfa,
-	0x1c, 0xc6, 0x2a, 0xbf, 0x94, 0xd2, 0xa2, 0x73, 0x6c, 0x10, 0xf8, 0x7f, 0xa1, 0xac, 0x77, 0x3b,
-	0xf7, 0x7e, 0xfd, 0x19, 0x85, 0x5f, 0x49, 0x16, 0x55, 0xf5, 0x2d, 0x89, 0x32, 0x18, 0xe5, 0xd6,
-	0x5c, 0xab, 0x14, 0xd9, 0x30, 0xd0, 0x26, 0xa4, 0xa7, 0x30, 0xdc, 0xa2, 0x96, 0xc6, 0xb2, 0x51,
-	0x00, 0x75, 0x74, 0xd8, 0xec, 0x51, 0x6b, 0xb3, 0xbf, 0x09, 0x3c, 0xbe, 0xb5, 0xd1, 0x13, 0x88,
-	0x94, 0x96, 0xf8, 0x2d, 0x78, 0x8c, 0x92, 0x2a, 0xb8, 0xd3, 0xe6, 0x09, 0x44, 0x3c, 0x55, 0xdc,
-	0xd5, 0x06, 0xab, 0xa0, 0x6b, 0x7e, 0x70, 0xdb, 0x7c, 0x0c, 0x90, 0x71, 0xd1, 0xb8, 0xaf, 0xdc,
-	0xb5, 0x94, 0xb2, 0x03, 0x97, 0x99, 0xd2, 0x57, 0x9e, 0xfb, 0xc2, 0x05, 0x83, 0x51, 0xd2, 0x96,
-	0xca, 0x0e, 0x26, 0x47, 0x5b, 0x27, 0x8c, 0x42, 0x42, 0x4b, 0x79, 0xf3, 0xfa, 0xe7, 0x3e, 0x26,
-	0xbf, 0xf6, 0x31, 0xf9, 0xbb, 0x8f, 0xc9, 0x87, 0x8b, 0x8d, 0xf2, 0x9f, 0x8a, 0xf5, 0x5c, 0x98,
-	0x6c, 0xb1, 0xe4, 0x9e, 0x2f, 0xcd, 0xa6, 0x3a, 0x86, 0xf3, 0x9c, 0xef, 0x52, 0xc3, 0x65, 0xf7,
-	0x54, 0xd6, 0xc3, 0x70, 0x25, 0x2f, 0xff, 0x05, 0x00, 0x00, 0xff, 0xff, 0x13, 0x71, 0x2b, 0x08,
-	0x4a, 0x03, 0x00, 0x00,
+	// 443 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x8c, 0x53, 0x4d, 0x6f, 0x13, 0x31,
+	0x10, 0xd5, 0x6e, 0xbb, 0x9b, 0x66, 0x22, 0x3e, 0x64, 0xaa, 0x62, 0x10, 0x5a, 0xa5, 0x39, 0xf5,
+	0xd2, 0xa4, 0x2a, 0x12, 0x47, 0xa4, 0x42, 0x2e, 0xb9, 0x00, 0xda, 0x72, 0xe2, 0x52, 0x39, 0xeb,
+	0xe9, 0x62, 0xd8, 0xd8, 0x2b, 0xdb, 0x09, 0x84, 0x1f, 0xc4, 0x6f, 0xe1, 0xc8, 0x89, 0x33, 0xca,
+	0x2f, 0x41, 0x19, 0x6f, 0xc2, 0x26, 0x80, 0xc4, 0xcd, 0xf3, 0xde, 0x9b, 0x37, 0x9a, 0x67, 0x0d,
+	0x9c, 0xd6, 0xd6, 0x78, 0x33, 0xd2, 0xe8, 0x3f, 0x19, 0xfb, 0xf1, 0x5c, 0xe2, 0x42, 0x15, 0xe8,
+	0x46, 0xa2, 0x44, 0xed, 0x87, 0xc4, 0xb1, 0x7b, 0x0d, 0x79, 0xd3, 0x90, 0x83, 0x37, 0x70, 0xf2,
+	0xd2, 0x54, 0x15, 0x16, 0xde, 0xd8, 0x57, 0x81, 0x1b, 0x13, 0xc5, 0x9e, 0x41, 0x1a, 0x44, 0x3c,
+	0xea, 0x47, 0x67, 0xbd, 0xcb, 0x6c, 0xb8, 0xd7, 0x3b, 0xdc, 0xd1, 0xe7, 0x8d, 0x7a, 0xf0, 0x05,
+	0x1e, 0xed, 0x3b, 0x4e, 0xb4, 0x47, 0x7b, 0x2b, 0x0a, 0x64, 0x8f, 0xe1, 0x28, 0xc8, 0x26, 0x92,
+	0x6c, 0xbb, 0xf9, 0xb6, 0x66, 0x57, 0x00, 0x6a, 0x23, 0x74, 0x3c, 0xee, 0x1f, 0x9c, 0xf5, 0x2e,
+	0x4f, 0xff, 0x35, 0x74, 0x6b, 0x99, 0xb7, 0x9a, 0x06, 0x5f, 0x63, 0xb8, 0xb3, 0xbb, 0xc5, 0x5d,
+	0x88, 0xd5, 0x66, 0x54, 0xac, 0x24, 0x7b, 0x08, 0x1d, 0x25, 0x6f, 0xbc, 0x28, 0xc3, 0x84, 0x6e,
+	0x9e, 0x2a, 0xf9, 0x56, 0x94, 0x8e, 0x31, 0x38, 0xd4, 0x62, 0x86, 0xfc, 0x80, 0xa4, 0xf4, 0x66,
+	0x7d, 0xe8, 0x49, 0x74, 0x85, 0x55, 0xb5, 0x57, 0x46, 0xf3, 0x43, 0xa2, 0xda, 0x10, 0x7b, 0x02,
+	0x5d, 0x55, 0x5f, 0x49, 0x69, 0xd1, 0x39, 0x9e, 0x10, 0xff, 0x1b, 0x58, 0xf7, 0xbb, 0xa5, 0x7b,
+	0x3d, 0xfd, 0x80, 0x85, 0x9f, 0x48, 0x9e, 0x86, 0xfe, 0x16, 0xc4, 0x38, 0x74, 0x6a, 0x6b, 0x6e,
+	0x55, 0x85, 0xbc, 0x43, 0xec, 0xa6, 0x64, 0x27, 0x90, 0x2e, 0x50, 0x4b, 0x63, 0xf9, 0x11, 0x11,
+	0x4d, 0xc5, 0x2e, 0xe0, 0x81, 0x98, 0x7b, 0x23, 0x95, 0x2b, 0xcc, 0x02, 0xed, 0xf2, 0x7a, 0x3e,
+	0xd5, 0xe8, 0x79, 0x97, 0x44, 0x7f, 0xa3, 0xd6, 0x9b, 0xd1, 0xbe, 0x40, 0xfb, 0xd2, 0x7b, 0xf0,
+	0x23, 0x82, 0xfb, 0x7f, 0x7c, 0xce, 0x31, 0x24, 0x4a, 0x4b, 0xfc, 0x4c, 0x71, 0x25, 0x79, 0x28,
+	0xb6, 0xc1, 0xc4, 0xad, 0x60, 0x8e, 0x21, 0x11, 0x95, 0x12, 0xae, 0x49, 0x2b, 0x14, 0xff, 0x11,
+	0x57, 0x06, 0x30, 0x13, 0xc5, 0x6e, 0x5e, 0x2d, 0x64, 0xed, 0x20, 0xe4, 0x4c, 0xe9, 0x6b, 0x2f,
+	0xfc, 0xdc, 0x51, 0x60, 0x49, 0xde, 0x86, 0xd6, 0x0e, 0xa6, 0x46, 0xdb, 0x08, 0x3a, 0x24, 0x68,
+	0x21, 0x2f, 0x9e, 0x7f, 0x5b, 0x65, 0xd1, 0xf7, 0x55, 0x16, 0xfd, 0x5c, 0x65, 0xd1, 0xbb, 0x8b,
+	0x52, 0xf9, 0xf7, 0xf3, 0xe9, 0xb0, 0x30, 0xb3, 0xd1, 0x58, 0x78, 0x31, 0x36, 0x65, 0x38, 0x87,
+	0xf3, 0x5a, 0x2c, 0x2b, 0x23, 0xe4, 0xfe, 0xb1, 0x4c, 0x53, 0xba, 0x93, 0xa7, 0xbf, 0x02, 0x00,
+	0x00, 0xff, 0xff, 0xb5, 0xeb, 0x56, 0xbc, 0x4c, 0x03, 0x00, 0x00,
 }
