314d5260b52413b1fb2f6507173c9872d58e7693
Derek Brown <derek.brown@datadoghq.com>
2021-09-09 14:43:47 -0700

    change lineendings for tests
    Implements new V2 protocol for encoding DNS domains.
    Ensures we only ever encode the same DNS domain once; all other
    references are indexes into an array of domain strings.
    
    add additional comments
    
    remove unused batchDomainIndexes
    
    Change encoding to include the "middle" offset for optimizing searches.
    Provide accessor to allow accessing individual string by index
    
    Change encoding to be by buffer offset rather than index
    
    add non-allocating string return

--- process/dns.go
+++ process/dns.go
@@ -1,61 +1,10 @@
 package process
 
 type DNSEncoder interface {
-	Encode(dns map[string]*DNSEntry) []byte
+	Encode(dns map[string]*DNSEntry) ([]byte, error)
+	EncodeMapped(dns map[string]*DNSDatabaseEntry, indexToOffset []int32) ([]byte, error)
+	EncodeDomainDatabase(names []string) ([]byte, []int32, error)
 }
 
 const dnsVersion1 byte = 1
-
-// GetDNS gets the DNS entries for the given IP from the given buffer
-func GetDNS(buf []byte, ip string) (string, []string) {
-	if len(buf) == 0 || ip == "" {
-		return "", nil
-	}
-
-	switch buf[0] {
-	case dnsVersion1:
-		return getV1(buf, ip)
-	}
-
-	return "", nil
-}
-
-func getDNSNames(buf []byte) []string {
-	if len(buf) == 0 {
-		return nil
-	}
-
-	switch buf[0] {
-	case dnsVersion1:
-		return getDNSNamesV1(buf)
-	}
-	return nil
-}
-
-// IterateDNS invokes the callback function for each DNS entry for the given IP in the given buffer
-func IterateDNS(buf []byte, ip string, cb func(i, total int, entry string) bool) error {
-	if len(buf) == 0 || ip == "" {
-		return nil
-	}
-
-	switch buf[0] {
-	case dnsVersion1:
-		return iterateDNSV1(buf, ip, cb)
-	}
-	return nil
-}
-
-// UnsafeIterateDNS invokes the callback function for each DNS entry for the given IP in the given buffer.
-// Each entry is a the slice from the overall buffer.  It should be copied before use
-func UnsafeIterateDNS(buf []byte, ip string, cb func(i, total int, entry []byte) bool) error {
-	if len(buf) == 0 || ip == "" {
-		return nil
-	}
-
-	switch buf[0] {
-	case dnsVersion1:
-		return unsafeIterateDNSV1(buf, ip, cb)
-	}
-
-	return nil
-}
+const dnsVersion2 byte = 2
