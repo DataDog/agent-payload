d6ff26aec408807194b5b62f11d84a848583cb05
Bryce Kahle <bryce.kahle@datadoghq.com>
2022-06-06 11:40:49 -0700

    Add IPAddress to ContainerAddr

--- process/addr.go
+++ process/addr.go
@@ -6,3 +6,14 @@ func (m *Addr) GetIP() IPAddress {
 	}
 	return *m.IpAddr
 }
+
+func (m *ContainerAddr) GetIP() IPAddress {
+	if m.IpAddr == nil {
+		return IPAddress{}
+	}
+	return *m.IpAddr
+}
+
+func (m IPAddress) IsZero() bool {
+	return m.Low == 0 && m.High == 0
+}
