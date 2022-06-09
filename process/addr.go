6021a0a3ba1431f7bc62d2214192d47e5edd6a77
Bryce Kahle <bryce.kahle@datadoghq.com>
2022-06-09 12:42:26 -0700

    Remove pointer helpers

--- process/addr.go
+++ process/addr.go
@@ -1,19 +1,5 @@
 package process
 
-func (m *Addr) GetIP() IPAddress {
-	if m.IpAddr == nil {
-		return IPAddress{}
-	}
-	return *m.IpAddr
-}
-
-func (m *ContainerAddr) GetIP() IPAddress {
-	if m.IpAddr == nil {
-		return IPAddress{}
-	}
-	return *m.IpAddr
-}
-
 func (m IPAddress) IsZero() bool {
 	return m.Low == 0 && m.High == 0
 }
