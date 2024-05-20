71c329e5f15dbeb0304d11a30e869500ac15605e
Hasan Mahmood <hasan.mahmood@datadoghq.com>
2024-05-20 16:18:06 -0500

    Add CCMEnabled field

--- process/connections.proto_builder.go
+++ process/connections.proto_builder.go
@@ -1466,6 +1466,13 @@ func (x *AgentConfigurationBuilder) SetDsmEnabled(v bool) {
 		x.writer.Write(x.scratch)
 	}
 }
+func (x *AgentConfigurationBuilder) SetCcmEnabled(v bool) {
+	if v {
+		x.scratch = protowire.AppendVarint(x.scratch[:0], 0x20)
+		x.scratch = protowire.AppendVarint(x.scratch, 1)
+		x.writer.Write(x.scratch)
+	}
+}
 
 type RouteBuilder struct {
 	writer        io.Writer
