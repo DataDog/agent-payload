5b36c015cb25f4803f00233a32a0a83543b69e40
Guy Arbitman <guy20495@gmail.com>
2025-08-21 13:59:14 +0300

    Simplify tests

--- process/tags_benchmarks_test.go
+++ process/tags_benchmarks_test.go
@@ -6,10 +6,45 @@ import (
 	"testing"
 )
 
+func generateExtremeDuplicationTags() [][]string {
+	uniqueTags := []string{"common", "shared", "reused", "frequent", "popular"}
+	var tagGroups [][]string
+	for i := 0; i < 10; i++ {
+		set := make([]string, 50)
+		for j := 0; j < 50; j++ {
+			set[j] = uniqueTags[j%5]
+		}
+		tagGroups = append(tagGroups, set)
+	}
+	return tagGroups
+}
+
+func generateMicroservicesRealisticTags() [][]string {
+	return [][]string{
+		{"service:web", "env:prod", "datacenter:us-east", "team:platform", "version:1.2.3"},
+		{"service:api", "env:prod", "datacenter:us-east", "team:platform", "version:1.2.3"},
+		{"service:db", "env:prod", "datacenter:us-east", "team:data", "version:1.2.3"},
+		{"service:cache", "env:prod", "datacenter:us-east", "team:platform", "version:1.2.3"},
+		{"service:queue", "env:prod", "datacenter:us-east", "team:messaging", "version:1.2.3"},
+		{"service:web", "env:staging", "datacenter:us-east", "team:platform", "version:1.2.4"},
+		{"service:api", "env:staging", "datacenter:us-east", "team:platform", "version:1.2.4"},
+		{"service:db", "env:staging", "datacenter:us-east", "team:data", "version:1.2.4"},
+	}
+}
+
+func generateXLarge100Tags() [][]string {
+	tags := make([]string, 100)
+	for i := 0; i < 100; i++ {
+		tags[i] = fmt.Sprintf("tag%03d:value%03d", i, i)
+	}
+	return [][]string{tags}
+}
+
 func BenchmarkTagEncodersAndDecoders(b *testing.B) {
 	files := []struct {
 		name  string
 		files []string
+		tags  [][]string
 	}{
 		{
 			name:  "low_dups",
@@ -28,16 +63,16 @@ func BenchmarkTagEncodersAndDecoders(b *testing.B) {
 			files: []string{"testdata/low_dups.txt", "testdata/high_dups.txt", "testdata/high_dups_2.txt"},
 		},
 		{
-			name:  "extreme_duplication",
-			files: []string{}, // Will use custom tag sets
+			name: "extreme_duplication",
+			tags: generateExtremeDuplicationTags(),
 		},
 		{
-			name:  "microservices_realistic",
-			files: []string{}, // Will use custom tag sets
+			name: "microservices_realistic",
+			tags: generateMicroservicesRealisticTags(),
 		},
 		{
-			name:  "xlarge_100_tags",
-			files: []string{}, // Will use custom tag sets
+			name: "xlarge_100_tags",
+			tags: generateXLarge100Tags(),
 		},
 	}
 
@@ -62,36 +97,9 @@ func BenchmarkTagEncodersAndDecoders(b *testing.B) {
 	for _, tt := range files {
 		var tagGroups [][]string
 
-		// Handle custom scenarios
-		if tt.name == "extreme_duplication" {
-			// 10 sets × 50 tags each, but only 5 unique tags total
-			uniqueTags := []string{"common", "shared", "reused", "frequent", "popular"}
-			for i := 0; i < 10; i++ {
-				set := make([]string, 50)
-				for j := 0; j < 50; j++ {
-					set[j] = uniqueTags[j%5] // Cycle through the 5 unique tags
-				}
-				tagGroups = append(tagGroups, set)
-			}
-		} else if tt.name == "microservices_realistic" {
-			tagGroups = [][]string{
-				{"service:web", "env:prod", "datacenter:us-east", "team:platform", "version:1.2.3"},
-				{"service:api", "env:prod", "datacenter:us-east", "team:platform", "version:1.2.3"},
-				{"service:db", "env:prod", "datacenter:us-east", "team:data", "version:1.2.3"},
-				{"service:cache", "env:prod", "datacenter:us-east", "team:platform", "version:1.2.3"},
-				{"service:queue", "env:prod", "datacenter:us-east", "team:messaging", "version:1.2.3"},
-				{"service:web", "env:staging", "datacenter:us-east", "team:platform", "version:1.2.4"},
-				{"service:api", "env:staging", "datacenter:us-east", "team:platform", "version:1.2.4"},
-				{"service:db", "env:staging", "datacenter:us-east", "team:data", "version:1.2.4"},
-			}
-		} else if tt.name == "xlarge_100_tags" {
-			tags := make([]string, 100)
-			for i := 0; i < 100; i++ {
-				tags[i] = fmt.Sprintf("tag%03d:value%03d", i, i)
-			}
-			tagGroups = [][]string{tags}
+		if tt.tags != nil {
+			tagGroups = tt.tags
 		} else {
-			// Handle file-based scenarios
 			for _, file := range tt.files {
 				tagGroups = append(tagGroups, readTestTags(b, file)...)
 			}
