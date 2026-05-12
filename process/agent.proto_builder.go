2c12a0a721b9858786df0c2132850cc0c423b94d
BarFinsdd <bar.fins@datadoghq.com>
2026-05-12 17:58:16 +0300

    Merge pull request #471 from DataDog/bar.fins/discovery-dep-map-poc
    Add discoveryServiceMapEnabled field to AgentConfiguration proto

--- process/agent.proto_builder.go
+++ process/agent.proto_builder.go
@@ -11043,6 +11043,11 @@ func (x *HTTPStats_DataBuilder) SetFirstLatencySample(v float64) {
 	x.scratch = protowire.AppendFixed64(x.scratch, math.Float64bits(v))
 	x.writer.Write(x.scratch)
 }
+func (x *HTTPStats_DataBuilder) SetLatencySum(v float64) {
+	x.scratch = protowire.AppendVarint(x.scratch[:0], 0x29)
+	x.scratch = protowire.AppendFixed64(x.scratch, math.Float64bits(v))
+	x.writer.Write(x.scratch)
+}
 
 type DNSDatabaseEntryBuilder struct {
 	writer  io.Writer
